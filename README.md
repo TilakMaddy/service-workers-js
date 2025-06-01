ec61a802ada2ecd3278c40b8b8d7a86fcbebed27
TilakMaddy <tilak.madichetti@gmail.com>
2025-06-01 16:11:54 +0530

    feat: chp4 minimal caching example

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Code is organized in branches.
 | Chapter 1 	| Skeleton for manually installed service worker 	| ch1-skeleton 	 |
 | Chapter 2 	| Important methods from Cache API	                | ch2-cache-api  |
 | Chapter 3 	| ev.waitUntil, self.skipWaiting, clients.Claim     | ch3-imp-methods|
+| Chapter 4 	| Minimal example for installing and cleaning cache | ch4-sw-minimal |
 
 
 > This repository is for personal reference. You should not use it in production.
--- src/service-worker.ts
+++ src/service-worker.ts
@@ -4,36 +4,33 @@
 /// <reference lib="WebWorker" />
 
 import { build, files, version } from '$service-worker';
+const CACHE = `cache-${version}`;
 
 declare const self: ServiceWorkerGlobalScope;
-declare const clients: Clients;
 
-const waitFor = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+// DO NOT include service-worker.js
+// CAN include manifest file (when building PWA)
+const ASSETS = [...build, ...files];
 
-/*
- * Post installation hook
- */
+// Install new cache
 self.addEventListener('install', (ev) => {
-	// directly calling `waitFor(100)` reserves the right to schedule the promise even after activation
-	ev.waitUntil(waitFor(1000));
-
-	/*
-	 * Normally, when there is a new service worker, the user has to click on `skipWaiting` after
-	 * navigating to dev tools > application > service workers.
-	 *
-	 * Or wait until all the tabs opened on that origin are closed to in order to activate
-	 *
-	 * With skipWaiting(), activation already happens. Meaning, new open tabs will be able to access the newly installed service
-	 * worker however the existing open tabs won't be able to switch their service worker to the new one. To do that, it is required
-	 * in the Service worker activate hook, for clients (tabs) to call `clients.claim()`.
-	 *
-	 */
-	self.skipWaiting(); // directly activate
+	ev.waitUntil(
+		new Promise<void>(async (resolve) => {
+			const cache = await caches.open(CACHE);
+			await cache.addAll(ASSETS);
+			resolve();
+		})
+	);
 });
 
+// Deletes old cache after activating new service worker with new cache installed
 self.addEventListener('activate', (ev) => {
-	clients.claim();
+	ev.waitUntil(
+		new Promise<void>(async (resolve) => {
+			for (const key of await caches.keys()) {
+				if (key !== CACHE) await caches.delete(key);
+			}
+			resolve();
+		})
+	);
 });
-
-// From what I gather, in general it's best to stay away from skipWaiting and claim because there can be breaking changes
-// which will become difficult to handle.
